c384d2d3d0ffe2709770d75fbe4b15a9e8e5743f
Ignacio Carol Lugones <icarol@fi.uba.ar>
2020-11-15 21:11:40 -0300

    Add initial paths and starting checks section

--- entrega.bash
+++ entrega.bash
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 #$GRUPO/so7508/pprincipal.log
-FILE_TO_LOGGER="./loggerFiles/logger.txt"
-#
+PATH_TO_LOGGER="./loggerFiles/logger.txt"
+#$GRUPO/input
 INPUT_PATH="./inputTest/"
+INPUT_ACCEPTED_PATH="${INPUT_PATH}ok/"
+#$GRUPO/rechazos
+REJECTED_PATH="./rechazos"
+#$DIRMAE/comercios.txt
+MERCHANT_REGISTER="${INPUT_PATH}/merchantForTest"
 OUTPUT_PLACE="./loggerFiles/"
 ACTUAL_CYCLE=1
 TIME_TO_SLEEP=5
 
-
 #this place to check for installation
+if [ ! -d "${INPUT_PATH}" ]; then
+  echo "error: Folder ${FILE_PATH} does not exist or you do not have access to it." >&2
+  exit 1
+fi
+
+#Ending check
 
 
 
